3999cf8741fced641d60dd3b34783450c22a9d78
merlinoa <merlinoa88@gmail.com>
2017-01-31 07:05:28 -0800

    moved zoneCreate functionality into the correct component

--- src/components/containers/Zones.jsx
+++ src/components/containers/Zones.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Zone from '../presentations/Zone';
-import ZoneAdd from '../presentations/ZoneAdd';
+import ZoneCreate from '../presentations/ZoneCreate';
 // helps with loading data from api
 import { APIManager } from '../../utils';
 
@@ -10,11 +10,6 @@ class Zones extends Component {
         super()
         
         this.state = {
-            zone: {
-                name: '',
-                zipCodes: ''
-            },
-            
             zoneList: []
         }
     }
@@ -33,34 +28,10 @@ class Zones extends Component {
         })
     }
     
-    // handles updates for both zone.name and zone.zipCodes
-    updateZoneHandler(event) {
-        let updatedZone = Object.assign({}, this.state.zone);
-        updatedZone[event.target.id] = event.target.value;
+    updateZoneList(zone) {
         
-        this.setState({
-            zone: updatedZone
-        });
-    }
-    
-    submitHandler(event) {
-        event.preventDefault();
-        
-        let newZone = this.state.zone;
-        // zips is a string of zip codes
-        let zips = newZone.zipCodes;
-        let zipsArray = zips.split(',');
-        
-        // trim possible white space around the zip codes
-        let newZips = [];
-        zipsArray.forEach(function(zipCode) {
-            newZips.push(zipCode.trim());
-        });
-        
-        newZone.zipCodes = newZips;
-        
-        // save comment to mongo
-        APIManager.post('/api/zone', newZone, (err, response) => {
+        // save zoneList state and save to mongo
+        APIManager.post('/api/zone', zone, (err, response) => {
             if (err) {
                 console.log("ERROR ZONE POST: " + err.message, null);
                 return
@@ -96,8 +67,7 @@ class Zones extends Component {
         
         return (
             <div>
-                <ZoneAdd submitHandler={this.submitHandler.bind(this)}
-                         updateZoneHandler={this.updateZoneHandler.bind(this)} />
+                <ZoneCreate updateZoneList={this.updateZoneList.bind(this)} />
                 <ul>
                     { listItems }
                 </ul>
---
+++ src/components/presentations/ZoneCreate.jsx
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+
+class ZoneCreate extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            zone: {
+                name: '',
+                zipCodes: ''
+            }
+        }
+    }
+    
+    // handles updates for both zone.name and zone.zipCodes
+    updateZoneHandler(event) {
+        let updatedZone = Object.assign({}, this.state.zone);
+        updatedZone[event.target.id] = event.target.value;
+        
+        this.setState({
+            zone: updatedZone
+        });
+    }
+
+    submitHandler(event) {
+        event.preventDefault();
+
+        let newZone = this.state.zone;
+        // zips is a string of zip codes
+        let zips = newZone.zipCodes;
+        let zipsArray = zips.split(',');
+        
+        // trim possible white space around the zip codes
+        let newZips = [];
+        zipsArray.forEach(function(zipCode) {
+            newZips.push(zipCode.trim());
+        });
+        
+        newZone.zipCodes = newZips;
+
+        this.props.updateZoneList(newZone);
+    }
+
+    render() {
+        return (
+            <form>
+                <div className='form-group'>
+                    <label>Zone Name</label>
+                    <input type="text"
+                           id="name" 
+                           placeholder="Zone Name"
+                           className="form-control"
+                           onChange={this.updateZoneHandler.bind(this)} />
+                </div>
+                <div className='form-group'>
+                    <label>Zip Code</label>
+                    <input type="text"
+                           id="zipCodes" 
+                           placeholder="Zone Name"
+                           className="form-control"
+                           onChange={this.updateZoneHandler.bind(this)} />
+                </div>
+                <button type="submit" 
+                    className="btn btn-primary"
+                    onClick={this.submitHandler.bind(this)}>
+                    Add Zone
+                </button>
+            
+            </form>
+        );
+    }
+};
+
+export default ZoneCreate;
