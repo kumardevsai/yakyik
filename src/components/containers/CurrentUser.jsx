62c66bb536e305fe73b920fcc08183e0f8d6d99b
merlinoa <merlinoa88@gmail.com>
2017-03-03 07:14:27 -0800

    current user profile

--- src/components/containers/CurrentUser.jsx
+++ src/components/containers/CurrentUser.jsx
@@ -3,11 +3,30 @@ import { connect } from 'react-redux';
 
 class CurrentUser extends Component {
     
+    constructor() {
+        super()
 
-    handleClick(event) {
-        event.preventDefault()
+        this.state = {
+            updated: {}
+        }
+    }
 
-        alert("Ouch!!")
+    updateCurrentUser(event) {
+        event.preventDefault();
+        let updated = Object.assign({}, this.state.updated);
+
+        updated[event.target.id] = event.target.value; 
+        
+        this.setState({
+            updated: updated
+        })
+        
+    }
+
+    updateProfile(event) {
+        event.preventDefault();
+
+        console.log('UpdateProfile: ' + JSON.stringify(this.state.updated));
     }
 
     render() {
@@ -16,11 +35,21 @@ class CurrentUser extends Component {
         return (
             <div>
                 <h1>{ "Howdy " + currentUser.username }</h1>
-                City: <input type="text" id ="city" placeholder={currentUser.city} />
+                City: <input 
+                    type="text" 
+                    id ="city" 
+                    placeholder={currentUser.city} 
+                    defaultValue={currentUser.city}
+                    onChange={this.updateCurrentUser.bind(this)}/>
                 <br />
-                City: <input type="text" id ="gender" placeholder={currentUser.gender} />
+                City: <input 
+                    type="text" 
+                    id ="gender" 
+                    placeholder={currentUser.gender} 
+                    defaultValue={currentUser.gender}
+                    onChange={this.updateCurrentUser.bind(this)}/>
                 <br />
-                <button type="submit" onClick={this.handleClick}>Submit</button>
+                <button type="submit" onClick={this.updateProfile.bind(this)}>Submit</button>
             </div>
         )
     }
